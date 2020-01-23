91882e8835ff49fd2ada7ae8c41fe911de175a7e
ivancao22 <58610497+ivancao22@users.noreply.github.com>
2020-01-23 16:40:22 -0300

    CREACION VECTOR "INDICE"

---
+++ pruebasVector-master/PruebasConVector/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PruebasConVector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVectormayor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGauss = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnBuscador = new System.Windows.Forms.Button();
+            this.lblindice = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnVectormayor
+            // 
+            this.btnVectormayor.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVectormayor.Location = new System.Drawing.Point(44, 12);
+            this.btnVectormayor.Name = "btnVectormayor";
+            this.btnVectormayor.Size = new System.Drawing.Size(107, 46);
+            this.btnVectormayor.TabIndex = 0;
+            this.btnVectormayor.Text = "mayor valor del vector";
+            this.btnVectormayor.UseVisualStyleBackColor = true;
+            this.btnVectormayor.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // btnGauss
+            // 
+            this.btnGauss.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGauss.Location = new System.Drawing.Point(44, 67);
+            this.btnGauss.Name = "btnGauss";
+            this.btnGauss.Size = new System.Drawing.Size(107, 46);
+            this.btnGauss.TabIndex = 2;
+            this.btnGauss.Text = "Suma de gauss faltante";
+            this.btnGauss.UseVisualStyleBackColor = true;
+            this.btnGauss.Click += new System.EventHandler(this.BtnGauss_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(44, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Bubble sort";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(201, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "label3";
+            // 
+            // btnBuscador
+            // 
+            this.btnBuscador.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscador.Location = new System.Drawing.Point(44, 210);
+            this.btnBuscador.Name = "btnBuscador";
+            this.btnBuscador.Size = new System.Drawing.Size(107, 46);
+            this.btnBuscador.TabIndex = 6;
+            this.btnBuscador.Text = "Buscador";
+            this.btnBuscador.UseVisualStyleBackColor = true;
+            this.btnBuscador.Click += new System.EventHandler(this.btnBuscador_Click);
+            // 
+            // lblindice
+            // 
+            this.lblindice.AutoSize = true;
+            this.lblindice.Location = new System.Drawing.Point(201, 227);
+            this.lblindice.Name = "lblindice";
+            this.lblindice.Size = new System.Drawing.Size(35, 13);
+            this.lblindice.TabIndex = 7;
+            this.lblindice.Text = "label4";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(47, 184);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtNumero.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 312);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.lblindice);
+            this.Controls.Add(this.btnBuscador);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnGauss);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVectormayor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVectormayor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGauss;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnBuscador;
+        private System.Windows.Forms.Label lblindice;
+        private System.Windows.Forms.TextBox txtNumero;
+    }
+}
+
